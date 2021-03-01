178b12cb514e220ebfe8668a97b6255e257f5501
tahmidmahdi <tahmid5550@gmail.com>
2021-03-01 18:50:52 +0600

    salary added

--- src/components/PlayerData/PlayerData.js
+++ src/components/PlayerData/PlayerData.js
@@ -2,11 +2,12 @@ import React from 'react';
 
 const playerData = (props) => {
     console.log('aaa', props.player);
-    const {name, img} = props.player;
+    const {name, img, salary} = props.player;
     return (
         <div>
             <img src={img} alt=""/>
             <h3>{name}</h3>
+            <h5>Salary: {salary}</h5>
         </div>
     );
 };
